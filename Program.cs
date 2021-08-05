0e64c8e1b03eeb032f59c5d731c63c9a60e10709
github <sagi.cohen37@gmail.com>
2021-08-05 23:09:36 +0300

    🎉 Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitlab_replicator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
